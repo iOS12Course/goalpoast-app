e41612c9e7aa33f4bc927ecc181a3cbfc88e1cf0
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-02-17 17:33:41 -0500

    Displaying static GoalCells in UiTableView

---
+++ goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by Ricardo Herrera Petit on 2/17/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType:String  {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
