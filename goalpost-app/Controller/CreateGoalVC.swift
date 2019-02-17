bb817377a33ae6fd1269629da8ac15e6c9999fde
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-02-17 17:56:19 -0500

    Building CreateGoalVC

---
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -0,0 +1,40 @@
+//
+//  CreateGoalVC.swift
+//  goalpost-app
+//
+//  Created by Ricardo Herrera Petit on 2/17/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var goalTextView: UITextView!
+    
+    @IBOutlet weak var shortTermBtn: UIButton!
+    
+    @IBOutlet weak var longTermBtn: UIButton!
+    
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func nextBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+    }
+    
+
+    @IBAction func longTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
